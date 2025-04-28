df6f9f6c15a811fba47c8fabb1b39a99bb91ddf5
Fannandya <fannandya.sutan.ft24@mail.umy.ac.id>
2025-04-28 11:15:02 +0700

    buat method buat input data

--- class2.cpp
+++ class2.cpp
@@ -5,3 +5,12 @@ class pp
 { // bikin class harus make uppercase
 private:
     int panjang, lebar;
+
+public:
+    void inputData()
+    {
+        cout << "masukkan panjangnya = ";
+        cin >> panjang;
+        cout << "masukkan lebarnya = ";
+        cin >> lebar;
+    };
