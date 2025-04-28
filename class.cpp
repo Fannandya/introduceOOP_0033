4147005385d10aeab9991ec009466031b4aca9f4
Fannandya <fannandya.sutan.ft24@mail.umy.ac.id>
2025-04-28 10:17:29 +0700

    membuat properti didalam class

--- class.cpp
+++ class.cpp
@@ -1,2 +1,23 @@
 #include <iostream>
 using namespace std;
+
+class Mobil
+{ // bikin class harus make uppercase
+public:
+    string namaMobil;
+    string tipeMobil;
+
+private:
+    int hargaMobil;
+
+public:
+    void inputData()
+    {
+        d
+    }
+};
+
+int main()
+{
+    w
+}
